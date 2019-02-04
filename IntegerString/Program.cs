93175630276c2deb38e847ad4a5fe73b5803e5f0
liveinmegacity <33742639+liveinmegacity@users.noreply.github.com>
2019-02-04 15:51:09 +0700

    Add project files.

---
+++ IntegerString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IntegerString
+{
+    class Program
+    {
+        public static string IntToString(int num)
+        {
+            if (num == 0) return "0";
+
+            bool negative = num < 0;
+            int delimiter = 10;
+            int exp;
+            string str = "";
+
+            while (num != 0)
+            {
+                exp = num % delimiter;
+                num = num / delimiter;
+
+                if (negative) exp = -exp;
+                str = exp + str;
+            }
+
+            return negative ? "-" + str : str;
+        }
+
+        static void Main(string[] args)
+        {
+            Test(0, "0");
+            Test(1234567890, "1234567890");
+            Test(int.MaxValue, "2147483647");
+            Test(int.MinValue, "-2147483648");
+        }
+
+        private static void Test(int num, string expectedResult)
+        {
+            var result = IntToString(num);
+            if (result == expectedResult)
+                Console.WriteLine(string.Format("{0} - OK", num));
+            else
+                Console.WriteLine(string.Format("{0} - failed. Expected: {1} but was {2}", num, expectedResult, result));
+        }
+    }
+}
